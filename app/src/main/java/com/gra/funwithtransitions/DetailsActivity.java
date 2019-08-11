d85fd2534d7daea0a5b0be22e4f11363c3b68ddf
Gabi <gabrielaradu.developer@gmail.com>
2019-08-11 13:06:52 +0300

    Show toolbar and back button action.

--- app/src/main/java/com/gra/funwithtransitions/DetailsActivity.java
+++ app/src/main/java/com/gra/funwithtransitions/DetailsActivity.java
@@ -3,6 +3,7 @@ package com.gra.funwithtransitions;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Toolbar toolbar = findViewById(R.id.detailsToolbar);
+        setSupportActionBar(toolbar);
+
+        // Show back icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
